4ac75ac48b05afb3db21dc0999a05d58546ecfff
Pratik <pratik1070061@gmail.com>
2021-02-07 18:21:05 -0500

    adding posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -14,10 +14,10 @@ const PostItem = ({
 }) => (
   <div className="post bg-white my-1 p-1">
     <div>
-      <a href="profile.html">
+      <Link to={`/profile/${user}`}>
         <img className="round-img" src={avatar} alt="" />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
 
     <div>
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,6 +3,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from "../actions/types";
 
 const initialState = {
@@ -22,6 +23,12 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false,
+      };
     case DELETE_POST:
       return {
         ...state,
@@ -35,7 +42,6 @@ export default function (state = initialState, action) {
         error: payload,
       };
     case UPDATE_LIKES:
-      console.log("here", payload);
       return {
         ...state,
         posts: state.posts.map((post) =>
