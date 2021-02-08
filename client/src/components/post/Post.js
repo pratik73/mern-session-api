aae76a6246a27a4d5299d0e98b14cd15a296297b
Pratik <pratik1070061@gmail.com>
2021-02-07 20:56:53 -0500

    adding comment display and delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,11 +6,12 @@ import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import { getPost } from "../../actions/post";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -21,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comment">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
