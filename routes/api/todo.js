b153469c450ed9529b6e25b196ee4ed5d6fcfa9b
Pratik <pratik1070061@gmail.com>
2022-01-25 00:56:02 -0500

    defining schema and routes

---
+++ routes/api/todo.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/todo/me
+// @desc    Get current users todos
+// @access  Private
+router.get("/me", async (req, res) => {
+  res.json({ msg: "Incomplete Implementation" });
+});
+
+// @route   POST api/todo
+// @desc    Create or update a todo
+// @access  Private
+router.post("/", async (req, res) => {
+  return res.send({ msg: "Incomplete Implementation" });
+});
+
+// @route   GET api/todo
+// @desc    Get all todos
+// @access  Public
+router.get("/", async (req, res) => {
+  return res.send({ msg: "Incomplete Implementation" });
+});
+
+// @route   DELETE api/todo
+// @desc    Delete todo, user
+// @access  Private
+router.delete("/:id", async (req, res) => {
+  return res.send({ msg: "Incomplete Implementation" });
+});
+
+module.exports = router;
